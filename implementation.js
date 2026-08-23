79ced0a96194403a92d036a09feeb57f0e922559
ThetPaing-7 <thatpainlin1999@gmail.com>
2026-08-24 00:12:23 +0630

    build: set function

--- implementation.js
+++ implementation.js
@@ -1,8 +1,11 @@
+import LinkListed from "./linkedList.js"
+
 class HashMap{
 
     
     load_factor = 0.75
     capacity = 16
+    buckets = new Array(this.capacity)
 
     constructor(){
 
@@ -14,13 +17,62 @@ class HashMap{
 
         const primeNumber = 7;
         for(let i = 0; i < key.length; i++){
-            hashCode = primeNumber * hashCode + key.charCodeAt(i)
-            hashCode = hashCode % this.capacity
+            hashCode += primeNumber * hashCode + key.charCodeAt(i)
+        }
+        // this.buckets[3] = 1
+        // console.log(this.buckets)
+        // console.log(this.buckets[3])
+        return hashCode % this.capacity
+    }
+
+    set(key,value){
+        let hashCode = this.hash(key)
+
+        console.log(hashCode)
+        if(this.buckets[hashCode] ===  undefined){
+            this.newLists = new LinkListed()
+           this.buckets[hashCode] = this.newLists.append({[key]: value})
+        }else{
+            let current = this.buckets[hashCode]
+
+               // Check if the key already exist
+            let found_key = false
+            while(current != null && found_key == false){
+                let exist_key = Object.keys(current.data)[0]
+                if(exist_key === key){
+                    current.data[exist_key] = value
+                    found_key = true
+                }
+                current = current.next
+            }    
+            
+            if (found_key === false){
+                this.newLists.append({[key]:value})
+            }
+
+
         }
 
-        return hashCode
     }
+
+
+    show(){
+        console.log(this.buckets)
+        for(let i = 0; i < this.buckets.length; i++){
+            console.log(this.buckets[i])
+        }
+    }
+
+
+
+   
+
 }
 
-let map = new HashMap()
-console.log(map.hash("Thet Paing Lin"))
+let map = new HashMap
+map.set("name","Thet")
+map.set("name","Lin")
+map.set("eman","Haydar")
+map.set("name","Paing")
+map.set("fruit","orange")
+map.show()
---
+++ linkedList.js
@@ -0,0 +1,193 @@
+class Node{
+    constructor(data){
+        this.data = data
+        this.next = null
+    } 
+}
+
+
+
+export default class LinkListed{
+    constructor(){
+        this.head = null
+    }
+
+
+    // add a node to the end of the list
+    append(value){
+        let node = new Node(value)
+        if(this.head == null){
+            this.head = node
+        }else{
+            let current = this.head
+            while(current.next != null){
+                current = current.next
+            }
+            current.next = node
+        }
+
+        return this.head
+    }
+
+    // add a node at the start of the list
+    prepend(value){
+        let node = new Node(value)
+        node.next = this.head
+        this.head = node
+    }
+
+    // return total number of node in a list
+    size(){
+        let current = this.head
+        let counter = 0
+        while(current != null){
+            counter++
+            current = current.next
+        }
+        return counter
+    }
+
+
+    // return first-node-value, if list empty, return undefined
+    first(){
+        let current = this.head
+        return current != null ? current.data : undefined
+    }
+
+    // retrun final-node-value, if list empty, return undefinded
+    tail(){
+        let current = this.head
+        
+        if(current != null){
+            let last_node = 0
+            while(current.next != null){
+                current = current.next
+            }
+            last_node = current
+            return last_node.data
+        }else{
+            return undefined
+        }
+        
+    }
+
+    //  return the value of the node at the given index, no node at given index return undefined
+    at(index){
+        let current = this.head
+        if(current != null){
+            let i = 0
+            while(i < index){
+                current = current.next
+                i++
+            }
+
+            return current.data
+        }else{
+            return undefined
+        }
+    }
+
+    // remove the first node of lists, if empty lits return undefined
+    pop(){
+        let current = this.head
+
+        if(current != null){
+            this.head = this.head.next
+            return current.data
+        }else{
+            return undefined
+        }
+    }
+
+    // return true if value is in lists else false
+    contain(value){
+        let current = this.head
+        while(current != null){
+            if(current.data === value){
+                return true
+            }
+
+            current = current.next
+        }
+        return false
+    }
+
+
+    // return index of node containing given value
+    // return -1 if not found, if more than index are found return first index
+    findIndex(value){
+        let current = this.head
+        let index = 0
+        while(current != null){
+            if(current.data === value){
+                return index
+            }
+            index++
+            current = current.next
+        }
+        return -1
+    }
+
+    toString(){
+        let current = this.head 
+        if(current == null){
+            return ""
+        }else{
+            let values = []
+            while(current != null){
+                values.push(`(${current.data})`)
+                current = current.next
+            }
+
+            return `${values.join("->")}->null`
+        }     
+    }
+
+
+    // list.insertAt(1, 10, 11)
+// console.log(list.toString());
+//=> "( 1 ) -> ( 10 ) -> ( 11 ) -> ( 2 ) -> ( 3 ) -> null"
+    insertAt(index,...values){
+
+    }
+
+    // remove node at given index, index is out of bound throw rangeError
+    removeAt(){
+
+    }
+
+    
+}
+
+let lists = new LinkListed()
+export {lists}
+// lists.append(10)
+// lists.append(13)
+// lists.prepend(3)
+// lists.prepend(7)
+
+
+
+// let current = lists.head
+// console.log(lists.head)
+
+// while(current != null){
+//     console.log(current.data)
+//     current = current.next
+// }
+
+
+// // console.log(lists.contain(0))
+// // console.log(lists.size())
+// // console.log(lists.first())
+// // console.log(lists.tail())
+// console.log("=============")
+// // console.log(lists.at(1))
+// console.log(lists.findIndex(10))
+// console.log(lists.findIndex(11))
+// console.log("=============")
+// console.log(lists.toString())
+// let second_lists = new LinkListed()
+// // console.log(second_lists.tail())
+// // console.log(second_lists.at(1))
+// // console.log(second_lists.contain(1))
